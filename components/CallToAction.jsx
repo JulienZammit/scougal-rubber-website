778ece30db4c7967ab90ed8d16808c79e54e624f
JulienZammit <julien.zammit@live.fr>
2024-12-22 13:01:02 +0100

    [fix] style button contact

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
+import { ImprovedModal, ModalButton, NavigationLink } from "../components/ui/modal-components";
 import Image from "next/image";
 
 const words = [
@@ -31,9 +32,9 @@ const CallToAction = () => {
           <TypewriterEffectSmooth words={words} />
           <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
             <a href="mailto:sales@scougalrubber.com" className="w-full">
-              <button className="bg-blue-500 text-white text-sm md:text-2xl px-6 py-2 rounded-[5px] border w-full hover:bg-slate-700 transition-all duration-300">
+              <ModalButton>
                 Contact Sales Now
-              </button>
+              </ModalButton>
             </a>
           </div>
         </div>
